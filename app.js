f9046e2d595454f803e7c6b205aca6d0d24c758e
georgia smite <georgiasmite@macbook-pro-de-georgia-smite.home>
2019-06-15 16:22:05 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var button = document.querySelector(".button");
+var menu = document.querySelector(".headerMenu");
+
+button.addEventListener("click", function () {
+    menu.classList.toggle("open");
+});
